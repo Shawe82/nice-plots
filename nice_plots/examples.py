41d218276569f3a266ef78e95b4c2f106c08929b
Simon Hawe <shawe@munichre.com>
2019-11-28 08:53:24 +0100

    Change value for stacked bars

--- nice_plots/examples.py
+++ nice_plots/examples.py
@@ -14,10 +14,11 @@ class Annotate:
     ax: matplotlib.axes.Axes
     font_size: int = 10
     color: str = "black"
+    n_dec: int = 2
 
     def horizontal(self):
         def get_vals(p: Patch) -> PosVal:
-            value = p.get_width() - p.get_x()
+            value = p.get_width()
             pos = (p.get_x() + p.get_width(), p.get_y() + p.get_height() / 2)
             return value, pos
 
@@ -25,7 +26,7 @@ class Annotate:
 
     def vertical(self):
         def get_vals(p: Patch) -> PosVal:
-            value = p.get_height() - p.get_y()
+            value = p.get_height()
             pos = (p.get_x() + p.get_width() / 2, p.get_y() + p.get_height())
             return value, pos
 
@@ -35,14 +36,14 @@ class Annotate:
         cfg = {"color": self.color, "fontsize": self.font_size, **kwargs}
         for p in self.ax.patches:
             value, pos = func(p)
-            self.ax.annotate(f"{value:.0f}", pos, **cfg)
+            self.ax.annotate(f"{value:.{self.n_dec}f}", pos, **cfg)
 
 
 def rotate_xticks(ax: matplotlib.axes, degrees=45):
     ax.set_xticklabels(ax.get_xticklabels(), rotation=degrees)
 
 
-def set_sizes(fig_size: Tuple[int, int] = [9, 6], font_size: int = 10):
+def set_sizes(fig_size: Tuple[int, int] = (9, 6), font_size: int = 10):
     plt.rcParams["figure.figsize"] = fig_size
     plt.rcParams["font.size"] = font_size
     plt.rcParams["xtick.labelsize"] = font_size
@@ -52,7 +53,7 @@ def set_sizes(fig_size: Tuple[int, int] = [9, 6], font_size: int = 10):
     plt.rcParams["legend.fontsize"] = font_size
 
 
-set_sizes([12, 8], 10)
+set_sizes((12, 8), 10)
 data = sns.load_dataset("iris").groupby("species").mean()
 fig, axes = plt.subplots(2, 2)
 data.plot.bar(ax=axes[0][0])
